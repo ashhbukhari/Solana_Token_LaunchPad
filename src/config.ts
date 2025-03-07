3338b8222e274a3058e8421c44345df9eb45cd9e
ashhbukhari <39802651+ashhbukhari@users.noreply.github.com>
2025-03-07 02:43:58 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const SOL_PRICE_API =
   "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
 
-export const SOLANA_RPC = process.env.NEXT_PUBLIC_SOLANA_RPC ?? "";
+export const SOLANA_RPC = process.env.NEXT_PUBLIC_SOLANA_RPC ?? "https://rpc.ankr.com/solana_devnet/b995cf3ee2ea4970e665b61c6a893dd3af7764417af5276cacb82d0c2743835a";
